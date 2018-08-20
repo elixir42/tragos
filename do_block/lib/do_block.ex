28b2cb5e1e4c4746e7d6a66c709cf12a5027cc8a
Luciano Ramalho <luciano@ramalho.org>
2018-08-20 17:51:53 -0600

    experimento com macro

--- do_block/lib/do_block.ex
+++ do_block/lib/do_block.ex
@@ -8,30 +8,30 @@ defmodule DoBlock do
 
   ## Examples
 
-      iex> DoBlock.dumpFn([{:a, 1}])
+      iex> DoBlock.dumpFn(:doit, [{:a, 1}])
       [a: 1]
-      iex> DoBlock.dumpFn([a: 1])
+      iex> DoBlock.dumpFn(:doit, [a: 1])
       [a: 1]
-      iex> DoBlock.dumpFn [a: 1]
+      iex> DoBlock.dumpFn :doit, [a: 1]
       [a: 1]
-      iex> DoBlock.dumpFn a: 1
+      iex> DoBlock.dumpFn :doit, a: 1
       [a: 1]
-      iex> DoBlock.dumpFn a: 1, do: DoBlock.hello()
-      [a: 1, do: :world]
-      iex> DoBlock.dumpFn do: DoBlock.hello()
+      iex> DoBlock.dumpFn :doit, do: DoBlock.hello()
       [do: :world]
-      iex> DoBlock.dumpFn do DoBlock.hello() end
+      iex> DoBlock.dumpFn :doit, do: DoBlock.hello()
       [do: :world]
-      iex> DoBlock.dumpFn do
+      iex> DoBlock.dumpFn :doit do DoBlock.hello() end
+      [do: :world]
+      iex> DoBlock.dumpFn :doit do
       ...>   DoBlock.hello()
       ...> end
       [do: :world]
-      iex> DoBlock.dumpFn do
+      iex> DoBlock.dumpFn :doit do
       ...>   IO.puts("Hello")
       ...>   DoBlock.hello()
       ...> end
       [do: :world]
-      iex> DoBlock.dumpFn do
+      iex> DoBlock.dumpFn :doit do
       ...>   IO.puts("Hello")
       ...>   DoBlock.hello()
       ...>   IO.puts("!")
@@ -39,8 +39,12 @@ defmodule DoBlock do
       [do: :ok]
 
   """
-  def dumpFn(arg) do
+  def dumpFn(:doit, arg) do
     arg
+  end
+
+  def dumpFn(:dont, arg) do
+    inspect(arg)
   end
 
   @doc """
@@ -55,7 +59,7 @@ defmodule DoBlock do
       iex> DoBlock.dumpMacro :doit, do: DoBlock.hello()
       [did: :world]
       iex> DoBlock.dumpMacro :dont, do: DoBlock.hello()
-      [didnot: "{{:., [line: 51], [{:__aliases__, [line: 51], [:DoBlock]}, :hello]}, [line: 51], []}"]
+      [didnot: "{{:., [line: 55], [{:__aliases__, [line: 55], [:DoBlock]}, :hello]}, [line: 55], []}"]
       iex> DoBlock.dumpMacro :doit do
       ...>   DoBlock.hello()
       ...> end
@@ -63,7 +67,7 @@ defmodule DoBlock do
       iex> DoBlock.dumpMacro :dont do
       ...>   DoBlock.hello()
       ...> end
-      [didnot: "{{:., [line: 52], [{:__aliases__, [line: 52], [:DoBlock]}, :hello]}, [line: 52], []}"]
+      [didnot: "{{:., [line: 56], [{:__aliases__, [line: 56], [:DoBlock]}, :hello]}, [line: 56], []}"]
 
   """
   defmacro dumpMacro(:doit, do: bloco) do
